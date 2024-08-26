ea48066a0fa8d4de8279378a92e3288372838dd6
spriveen <rpeanuka@gmail.com>
2024-08-25 20:39:12 -0700

    update Header.tsx

--- job-board-app-with-nextjs/src/app/page.tsx
+++ job-board-app-with-nextjs/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Hero from "./components/Hero";
+
+
+
 export default function Home() {
   return (
-    <div></div>
+    
+    <Hero />
     
   );
 }
--- job-board-app-with-nextjs/tailwind.config.ts
+++ job-board-app-with-nextjs/tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '100px',
+        md: '200px'
+        
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
